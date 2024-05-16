458ea5a44554e343fd672062fb55adcad9ff3388
UIT20521227 <116501582+TruongDucDuong1109@users.noreply.github.com>
2024-05-16 10:47:32 +0700

    update  cv2

--- capture.py
+++ capture.py
@@ -1,85 +1,3 @@
-# import sys
-# import cv2
-# from PyQt5 import QtCore
-# from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread
-# from PyQt5.QtGui import QImage, QPixmap
-# from PyQt5.QtWidgets import QDialog, QApplication
-# from PyQt5.uic import loadUi
-
-# class VideoThread(QThread):
-#     change_pixmap_signal = pyqtSignal(QImage)
-
-#     def __init__(self):
-#         super().__init__()
-#         self._run_flag = True
-#         self.capture = cv2.VideoCapture('http://10.10.22.40:8080/video')
-
-#     def run(self):
-#         while self._run_flag:
-#             ret, cv_img = self.capture.read()
-#             if ret:
-#                 print("Received frame from camera.")  # In thông báo nếu nhận được khung hình từ camera
-#                 qt_img = self.convert_cv_qt(cv_img)
-#                 self.change_pixmap_signal.emit(qt_img)
-#             else:
-#                 print("Failed to receive frame from camera.")  # In thông báo nếu không nhận được khung hình từ camera
-#                 # Đồng thời, bạn cũng có thể in thông báo lỗi chi tiết nếu cần
-
-#     def stop(self):
-#         self._run_flag = False
-#         self.capture.release()
-#         self.quit()
-
-#     def convert_cv_qt(self, cv_img):
-#         rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-#         h, w, ch = rgb_image.shape
-#         bytes_per_line = ch * w
-#         convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
-#         p = convert_to_Qt_format.scaled(640, 480, QtCore.Qt.KeepAspectRatio)
-#         return p
-
-# class TehseenCode(QDialog):
-#     def __init__(self):
-#         super(TehseenCode, self).__init__()
-#         loadUi('loadui.ui', self)
-#         self.logic = 0
-#         self.value = 1
-#         self.SHOW.clicked.connect(self.start_video)
-#         self.TEXT.setText('Press "SHOW" to connect with camera')
-#         self.CAPTURE.clicked.connect(self.CaptureClicked)
-#         self.QUIT.clicked.connect(self.quitClicked)
-#         self.thread = None
-
-#     @pyqtSlot()
-#     def start_video(self):
-#         self.thread = VideoThread()
-#         self.thread.change_pixmap_signal.connect(self.update_image)
-#         self.thread.start()
-
-#     @pyqtSlot(QImage)
-#     def update_image(self, img):
-#         self.imgLabel.setPixmap(QPixmap.fromImage(img))
-
-#     def CaptureClicked(self):
-#         if self.thread is not None:
-#             frame = self.thread.capture.read()[1]
-#             if frame is not None:
-#                 cv2.imwrite(f"image{self.value}.jpg", frame)
-#                 self.TEXT.setText('Image captured')
-#                 self.value += 1
-
-#     def quitClicked(self):
-#         if self.thread is not None:
-#             self.thread.stop()
-#         sys.exit()
-
-# app = QApplication(sys.argv)
-# window = TehseenCode()
-# window.show()
-# sys.exit(app.exec_())
-
-
-
 import sys
 import cv2
 import os
@@ -97,7 +15,9 @@ class VideoThread(QThread):
     def __init__(self):
         super().__init__()
         self._run_flag = True
-        self.capture = cv2.VideoCapture('http://10.10.22.40:8080/video')
+        # self.capture = cv2.VideoCapture('http://10.10.22.40:8080/video')
+        self.capture = cv2.VideoCapture(1)
+        
         self.current_frame = None
 
     def run(self):
